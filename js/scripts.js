fcbcce832e904f6f123b09c88621231e994d0aa7
mickijames <micalynjeanette@eagles.ewu.edu>
2017-03-14 14:42:22 -0700

    Thumbnail properties
    * rearranged pictures
    * added js for thumbnail

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 // Activate Light Gallery
 $('figure').lightGallery({
     selector: 'a',
-    thumbnail: false,
+    thumbnail: true,
+    animateThumb: true,
+    showThumbByDefault: false,
     escKey: true,
     keyPress: true,
     closeable: true
